fcabf6c2282ed4cabc9e4f5e4f6e08ccf31e3b69
JuliaSuhovici <63416383+JuliaSuhovici@users.noreply.github.com>
2020-05-14 16:20:06 +0300

    Update RoboInheritanceExample.java

--- src/main/RoboInheritanceExample.java
+++ src/main/RoboInheritanceExample.java
@@ -5,7 +5,8 @@ public class RoboInheritanceExample {
 
 	public static void main(String[] args) {
 		
-		System.out.printf( "Simple test passed %d%%\n", SimpleTest.runCase( new AlphaRobot("Robik","alpha") ) );
+		System.out.printf( "Simple test passed %d%%\n", SimpleTest.runCase( new BetaRobot("Robo","beta") ) );
+		System.out.printf( "Charge test passed %d%%\n", ChargeTest.runCase( new BetaRobot("Robo","beta") ) );
 		
 	}
 }
