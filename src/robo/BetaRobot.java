b63d15935b63ad2200d90d611edf0d236f55055f
JuliaSuhovici <63416383+JuliaSuhovici@users.noreply.github.com>
2020-05-14 16:16:28 +0300

    Add files via upload

---
+++ src/robo/BetaRobot.java
@@ -0,0 +1,78 @@
+package robo;
+
+public class BetaRobot extends AlphaRobot{
+	private byte charge;
+	private byte walk;
+
+	//-------CONSTRUCTORS------//
+	public BetaRobot() {}
+	public BetaRobot(String name, String model) {
+		super(name, model);
+	}
+
+	//-------GETTER------//
+	public byte getCharge() {
+		return charge;
+	}
+
+	//-------SETTER------//
+	public void setCharge(int charge) {
+		if(charge >=0 && charge <=100)
+			this.charge = (byte)charge;
+		else
+			System.err.println("WRONG VALUE OF CHARGE!");
+	}
+	
+	//-------MOVING------//
+	@Override
+	public boolean moveRight() {
+		int newX = getX()+1;
+		if(checkCoords(newX) && charge >5) {
+			setX(newX);
+			checkCharge();
+			return true;
+		}
+		else return false;
+	}
+	
+	@Override
+    public boolean moveLeft() {
+    	int newX = getX()-1;;
+    	if(checkCoords(newX) && charge >5 ) {
+			setX(newX);
+			checkCharge();
+			return true;
+		}
+		else return false;
+    }
+	
+	@Override
+    public boolean moveDown() {
+    	int newY = getY()+1;
+    	if(checkCoords(newY) && charge >5) {
+			setY(newY);
+			checkCharge();
+			return true;
+		}
+		else return false; 
+    }
+	
+	@Override
+    public boolean moveUp() {
+    	int newY = getY()-1;
+    	if(checkCoords(newY) && charge >5) {
+			setY(newY);
+			checkCharge();
+			return true;
+		}
+		else return false; 
+    }
+
+    protected void checkCharge(){
+    	walk++;
+    	if(walk == 2 ) {
+    		setCharge(--charge);
+    		walk = 0;
+    	}	
+    }
+}
