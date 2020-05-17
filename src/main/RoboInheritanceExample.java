32e933e48fbe6dbbf72f4f14b3a1ad3b65946463
JuliaSuhovici <63416383+JuliaSuhovici@users.noreply.github.com>
2020-05-17 09:17:26 +0300

    Update RoboInheritanceExample.java

--- src/main/RoboInheritanceExample.java
+++ src/main/RoboInheritanceExample.java
@@ -4,9 +4,11 @@ import tests.*;
 public class RoboInheritanceExample {
 
 	public static void main(String[] args) {
-		
-		System.out.printf( "Simple test passed %d%%\n", SimpleTest.runCase( new BetaRobot("Robo","beta") ) );
-		System.out.printf( "Charge test passed %d%%\n", ChargeTest.runCase( new BetaRobot("Robo","beta") ) );
-		
+		//-----CHARLIE TESTS------//	
+	//	System.out.printf( "Simple test passed %d%%\n", SimpleTest.runCase( new CharlieRobot("Robo","charlie")  ) );
+	//	System.out.printf( "Charge test passed %d%%\n", ChargeTest.runCase( new CharlieRobot("Robo","charlie")  ) );
+	//	System.out.printf( "Walk test passed %d%%\n", DiagonalWalkTest.runCase( new CharlieRobot("Robo","charlie") ) );
+		//-----FACTORY PATTERN------//	
+		AbstractRobot robo = RobotFactory.getRobot("beta","Bobo"); 
 	}
 }
